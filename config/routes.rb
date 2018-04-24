896fe6d19739647bac9337644f19e09b64754dac
Kisei Takinishi <Kisei@Kisei-no-MacBook-Air.local>
2018-04-24 18:52:10 +0900

    24th Apr 2018

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   root 'home#top'
   get  '/signup',  to: 'users#new'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
   get "about" => "home#about"

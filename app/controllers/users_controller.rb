162d886aa396e8cc7ebe14d08ccf7bcd1959cdc1
Kisei Takinishi <Kisei@Kisei-no-MacBook-Air.local>
2018-05-14 17:08:33 +0900

    postgresql

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,30 +33,30 @@ class UsersController < ApplicationController
   #   end
   # end
 
-  def create
-      if env['omniauth.auth'].present?
-           # Facebookログイン
-           @user  = User.from_omniauth(env['omniauth.auth'])
-           result = @user.save(context: :facebook_login)
-           fb       = "Facebook"
-       else
-           # 通常サインアップ
-           @user  = User.new(user_params)
-           result = @user.save
-           fb       = ""
-       end
-       if result
-           sign_in @user
-           flash[:success] = "#{fb}ログインしました。"
-           redirect_to @user
-       else
-           if fb.present?
-               redirect_to auth_failure_path
-           else
-               render 'new'
-           end
-       end
-   end
+  # def create
+  #     if env['omniauth.auth'].present?
+  #          # Facebookログイン
+  #          @user  = User.from_omniauth(env['omniauth.auth'])
+  #          result = @user.save(context: :facebook_login)
+  #          fb       = "Facebook"
+  #      else
+  #          # 通常サインアップ
+  #          @user  = User.new(user_params)
+  #          result = @user.save
+  #          fb       = ""
+  #      end
+  #      if result
+  #          sign_in @user
+  #          flash[:success] = "#{fb}ログインしました。"
+  #          redirect_to @user
+  #      else
+  #          if fb.present?
+  #              redirect_to auth_failure_path
+  #          else
+  #              render 'new'
+  #          end
+  #      end
+  #  end
 
   def edit
     @user = User.find(params[:id])
